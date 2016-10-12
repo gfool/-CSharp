7d2ff87d94316d9a01e22436d9ed0b26cf2e8236
gfool <gfool@SC-201610041925>
2016-10-12 13:09:06 +0800

    添加项目文件。

---
+++ Collecitons/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collecitons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strArray = { "First", "Second", "Third" };
+
+            Console.WriteLine("This array has {0} items.", strArray.Length);
+
+            foreach(string s in strArray)
+            {
+                Console.WriteLine("Array Entry: {0}.", s);
+            }
+            Console.WriteLine();
+
+            // 颠倒数组
+            Array.Reverse(strArray);
+
+            foreach(string s in strArray)
+            {
+                Console.WriteLine("Array Entry: {0}.", s);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
